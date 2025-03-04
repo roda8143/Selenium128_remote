92fe70766f6827e911fd16c2ccccc3d14ce9055b
roda8143 <roda8143@gmail.com>
2025-03-04 13:17:28 +0530

    first commit

---
+++ src/main/java/Selenium128/Selenium128/App.java
@@ -0,0 +1,10 @@
+package Selenium128.Selenium128;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
